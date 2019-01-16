3a000bcd521a5b732e7371df3067bc1279f3835b
Trem <tremaynechrist@gmail.com>
2019-01-16 13:59:13 +0000

    add listeners automatically based on event name

--- src/DOMInteractions.ts
+++ src/DOMInteractions.ts
@@ -1,36 +1,40 @@
 type StandardCallback = () => any;
 const watchers: StandardCallback[] = [];
 
+const events = [
+  // Global Resize
+  'resize',
+  // Transitions & Animations
+  'transitionend',
+  'animationend',
+  'animationstart',
+  'animationiteration',
+  // Interactions
+  'keyup',
+  'keydown',
+  'mouseup',
+  'mousedown',
+  'mouseover',
+  'blur',
+  'focus'
+];
+
+// Dispatcher for interactions
 const dispatch: StandardCallback = () => {
   watchers.forEach((watcher: StandardCallback) => watcher());
+}
+
+// Listen to events
+events.forEach(name => window.addEventListener(name, dispatch, true));
+
+// Listen for any other DOM changes which could affect sizes
+if ('MutationObserver' in window) {
+  const observerConfig = { attributes: true, characterData: true, childList: true, subtree: true };
+  new MutationObserver(dispatch).observe(document.body, observerConfig);
 }
 
 export default class DOMInteractions {
   public static watch (callback: StandardCallback) {
     watchers.push(callback);
   }
-}
-
-// Listen to interaction
-document.addEventListener('keyup', dispatch, true);
-document.addEventListener('keydown', dispatch, true);
-document.addEventListener('mouseup', dispatch, true);
-document.addEventListener('mousedown', dispatch, true);
-document.addEventListener('mouseover', dispatch, true);
-document.addEventListener('blur', dispatch, true);
-document.addEventListener('focus', dispatch, true);
-
-// Listen to transitions and animations
-document.addEventListener('transitionend', dispatch, true);
-document.addEventListener('animationend', dispatch, true);
-document.addEventListener('animationstart', dispatch, true);
-document.addEventListener('animationiteration', dispatch, true);
-
-// Listen for window resize
-window.addEventListener('resize', dispatch);
-
-// Listen for any other DOM changes which could affect sizes
-if ('MutationObserver' in window) {
-  const observerConfig = { attributes: true, characterData: true, childList: true, subtree: true };
-  new MutationObserver(dispatch).observe(document.body, observerConfig);
 }
