6937271db677f9cfaf3057c61938a45629d43f47
Trem <tremaynechrist@gmail.com>
2019-01-30 22:09:24 +0000

    commented algorithms and linked to relevant areas of the specification

--- src/algorithms/hasActiveObservations.ts
+++ src/algorithms/hasActiveObservations.ts
@@ -1,6 +1,11 @@
 import { resizeObservers } from '../ResizeObserverController';
 import { ResizeObserverDetail } from '../ResizeObserverDetail';
 
+/**
+ * Checks to see if there are any active observations.
+ * 
+ * https://drafts.csswg.org/resize-observer-1/#has-active-observations-h
+ */
 const hasActiveObservations = (): boolean => {
   return resizeObservers.some((ro: ResizeObserverDetail) => ro.activeTargets.length > 0);
 }
--- src/algorithms/hasSkippedObservations.ts
+++ src/algorithms/hasSkippedObservations.ts
@@ -1,6 +1,12 @@
 import { resizeObservers } from '../ResizeObserverController';
 import { ResizeObserverDetail } from '../ResizeObserverDetail';
 
+/**
+ * Checks to see if there are any skipped observations.
+ * This is used to deliver the resize loop error.
+ * 
+ * https://drafts.csswg.org/resize-observer-1/#has-skipped-observations-h
+ */
 const hasSkippedObservations = (): boolean => {
   return resizeObservers.some((ro: ResizeObserverDetail) => ro.skippedTargets.length > 0);
 }
