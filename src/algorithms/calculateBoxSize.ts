e139165a4af4b1e5d82e11aa1bc72ee1b9c58dbc
Trem <tremaynechrist@gmail.com>
2019-02-09 16:41:34 +0000

    tidy up calculateBoxSize algo for hidden states

--- src/algorithms/calculateBoxSize.ts
+++ src/algorithms/calculateBoxSize.ts
@@ -15,6 +15,20 @@ const cache = new Map();
 const IE = (/msie|trident/i).test(navigator.userAgent);
 const parseDimension = (pixel: string | null): number => parseFloat(pixel || '0');
 
+// Helper to generate and freeze a ResizeObserverSize
+const size = (inlineSize: number = 0, blockSize: number = 0): ResizeObserverSize => {
+  return Object.freeze({ inlineSize, blockSize });
+}
+
+// Return this when targets are hidden
+const zeroBoxes = Object.freeze({
+  borderBoxSize: size(),
+  contentBoxSize: size(),
+  scrollBoxSize: size(),
+  devicePixelBorderBoxSize: size(),
+  contentRect: new DOMRectReadOnly(0, 0, 0, 0)
+})
+
 /**
  * Gets all box sizes of an element.
  */
@@ -25,70 +39,49 @@ const calculateBoxSizes = (target: Element): ResizeObserverSizeCollection => {
     return cache.get(target);
   }
 
-  // If element is an SVG, handle things differently, using its bounding box.
-  const svg = isSVG(target) && (target as SVGGraphicsElement).getBBox();
+  // If the target is hidden, send zero
+  if (isHidden(target)) {
+    cache.set(target, zeroBoxes);
+    return zeroBoxes;
+  }
 
   const cs = getComputedStyle(target);
+  const dpr = window.devicePixelRatio;
+
+  // If element is an SVG, handle things differently, using its bounding box.
+  const svg = isSVG(target) && (target as SVGGraphicsElement).getBBox();
 
   // IE does not remove padding from width/height, when box-sizing is border-box.
   const removePadding = !IE && cs.boxSizing === 'border-box';
 
   // Calculate properties for creating boxes.
-  const hidden = isHidden(target);
-  const paddingTop = svg || hidden ? 0 : parseDimension(cs.paddingTop);
-  const paddingRight = svg || hidden ? 0 : parseDimension(cs.paddingRight);
-  const paddingBottom = svg || hidden ? 0 : parseDimension(cs.paddingBottom);
-  const paddingLeft = svg || hidden ? 0 : parseDimension(cs.paddingLeft);
-  const borderTop = svg || hidden ? 0 : parseDimension(cs.borderTopWidth);
-  const borderRight = svg || hidden ? 0 : parseDimension(cs.borderRightWidth);
-  const borderBottom = svg || hidden ? 0 : parseDimension(cs.borderBottomWidth);
-  const borderLeft = svg || hidden ? 0 : parseDimension(cs.borderLeftWidth);
+  const paddingTop = svg ? 0 : parseDimension(cs.paddingTop);
+  const paddingRight = svg ? 0 : parseDimension(cs.paddingRight);
+  const paddingBottom = svg ? 0 : parseDimension(cs.paddingBottom);
+  const paddingLeft = svg ? 0 : parseDimension(cs.paddingLeft);
+  const borderTop = svg ? 0 : parseDimension(cs.borderTopWidth);
+  const borderRight = svg ? 0 : parseDimension(cs.borderRightWidth);
+  const borderBottom = svg ? 0 : parseDimension(cs.borderBottomWidth);
+  const borderLeft = svg ? 0 : parseDimension(cs.borderLeftWidth);
   const horizontalPadding = paddingLeft + paddingRight;
   const verticalPadding = paddingTop + paddingBottom;
   const horizontalBorderArea = borderLeft + borderRight;
   const verticalBorderArea = borderTop + borderBottom;
   const widthReduction = removePadding ? horizontalPadding + horizontalBorderArea : 0;
   const heightReduction = removePadding ? verticalPadding + verticalBorderArea : 0;
-  const contentWidth = hidden ? 0 : svg ? svg.width : parseDimension(cs.width) - widthReduction;
-  const contentHeight = hidden ? 0 : svg ? svg.height : parseDimension(cs.height) - heightReduction;
+  const contentWidth = svg ? svg.width : parseDimension(cs.width) - widthReduction;
+  const contentHeight = svg ? svg.height : parseDimension(cs.height) - heightReduction;
+  const borderBoxWidth = contentWidth + horizontalPadding + horizontalBorderArea;
+  const borderBoxHeight = contentHeight + verticalPadding + verticalBorderArea;
 
-  // Create borderBoxSize
-  const borderBoxSize: ResizeObserverSize = {
-    inlineSize: contentWidth + horizontalPadding + horizontalBorderArea,
-    blockSize: contentHeight + verticalPadding + verticalBorderArea
-  }
+  const boxes = Object.freeze({
+    borderBoxSize: size(borderBoxWidth, borderBoxHeight),
+    contentBoxSize: size(contentWidth, contentHeight),
+    scrollBoxSize: size(contentWidth + horizontalPadding, contentHeight + verticalPadding),
+    devicePixelBorderBoxSize: size(borderBoxWidth * dpr, borderBoxHeight * dpr),
+    contentRect: new DOMRectReadOnly(paddingLeft, paddingTop, contentWidth, contentHeight)
+  })
 
-  // Create contentSize
-  const contentBoxSize: ResizeObserverSize = {
-    inlineSize: contentWidth,
-    blockSize: contentHeight
-  }
-
-  // Create scrollSize
-  const scrollBoxSize: ResizeObserverSize = {
-    inlineSize: contentWidth + horizontalPadding,
-    blockSize: contentHeight + verticalPadding
-  }
-
-  // Create devicePixelBorderBoxSize
-  const devicePixelBorderBoxSize: ResizeObserverSize = {
-    inlineSize: borderBoxSize.inlineSize * window.devicePixelRatio,
-    blockSize: borderBoxSize.blockSize * window.devicePixelRatio
-  }
-
-  // Create legacy contentRect
-  const contentRect = new DOMRectReadOnly(paddingLeft, paddingTop, contentWidth, contentHeight);
-
-  const boxes = {
-    borderBoxSize,
-    contentBoxSize,
-    scrollBoxSize,
-    devicePixelBorderBoxSize,
-    contentRect
-  };
-
-  // Cache the boxes as there will
-  // be multiple requests for information.
   cache.set(target, boxes);
 
   return boxes;
