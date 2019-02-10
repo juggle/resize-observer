58830305ae97a167e2e70234dbc4f97fbfe1726d
Tremayne Christ <info@tremaynechrist.co.uk>
2019-02-10 15:36:43 +0000

    fix: return infitite depth for a node which is hidden (#33)

--- src/algorithms/calculateDepthForNode.ts
+++ src/algorithms/calculateDepthForNode.ts
@@ -1,9 +1,13 @@
+import { isHidden } from '../utils/element';
 /**
  * Calculates the depth of a node.
  * 
  * https://drafts.csswg.org/resize-observer-1/#calculate-depth-for-node-h
  */
 const calculateDepthForNode = (node: Element): number => {
+  if (isHidden(node)) {
+    return Infinity;
+  }
   let depth = 0;
   let parent = node.parentNode;
   while (parent) {
