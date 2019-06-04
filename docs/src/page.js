2bcb7cc1c324a76b51374cdacac5830124c80eda
Trem <tremaynechrist@gmail.com>
2019-06-04 18:35:31 +0100

    Add docs src

---
+++ docs/src/page.js
@@ -0,0 +1,44 @@
+import ResizeObserver from '../../src/ResizeObserver.ts';
+
+const perfArea = document.getElementById('performance-example');
+const perfFragment = document.createDocumentFragment();
+const perfCount = document.getElementById('performance-count');
+let ticks = 0;
+
+const ro = new ResizeObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.target.parentElement === perfArea) {
+      ticks += 1;
+      perfCount.innerText = ticks;
+      return;
+    }
+    const { inlineSize, blockSize } = entry.contentBoxSize;
+    entry.target.setAttribute('dimensions', `${Math.round(inlineSize)} x ${Math.round(blockSize)}`);
+  });
+});
+
+[...document.querySelectorAll('pre, code')].forEach(el => {
+  el.innerHTML = el.innerHTML.trim();
+});
+
+for (let i = 0; i < 200; i += 1) {
+  const el = document.createElement('div');
+  el.setAttribute('resize', '');
+  perfFragment.appendChild(el);
+}
+
+perfArea.appendChild(perfFragment);
+
+perfArea.addEventListener('click', function () {
+  this.toggleAttribute('animate');
+});
+
+document.getElementById('transition-example').addEventListener('click', function () {
+  this.toggleAttribute('fill');
+});
+
+document.getElementById('animation-example').addEventListener('click', function () {
+  this.toggleAttribute('animate');
+});
+
+[...document.querySelectorAll('[resize]')].forEach(el => ro.observe(el));
