005ae14c90719ad26e4970d1ef631f2d0fbb29a5
Trem <tremaynechrist@gmail.com>
2019-07-02 17:07:08 +0100

    add rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,29 @@
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
 
-export default {
-  input: 'lib/ResizeObserver',
+const onwarn = (warning) => {
+  if (warning.code !== 'CIRCULAR_DEPENDENCY') {
+    console.error(`(!) ${warning.message}`);
+  }
+};
+
+export default [{
+  input: 'src/ResizeObserver.ts',
+  output: {
+    file: './lib/ResizeObserver.js',
+    format: 'esm'
+  },
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+      clean: true
+    })
+  ],
+  onwarn
+},
+{
+  input: 'src/ResizeObserver.ts',
   output: {
     file: './umd/ResizeObserver.js',
     exports: 'named',
@@ -10,12 +31,20 @@ export default {
     format: 'umd'
   },
   plugins: [
-    resolve(),
+    typescript({
+      typescript: require('typescript'),
+      clean: true
+    }),
     babel({
-      exclude: 'node_modules/**',
       'presets': [
         ['@babel/env', { 'modules' : false }]
+      ],
+      extensions: [
+        ...DEFAULT_EXTENSIONS,
+        'ts',
+        'tsx'
       ]
     })
-  ]
-}
+  ],
+  onwarn
+}]
