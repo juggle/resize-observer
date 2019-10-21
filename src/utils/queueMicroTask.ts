fa242d044028aca0aad33f2c69db8e0d736c925a
Trem <tremaynechrist@gmail.com>
2019-10-21 08:36:54 +0100

    Update queueMicroTask to support multiple callbacks

--- src/utils/queueMicroTask.ts
+++ src/utils/queueMicroTask.ts
@@ -1,33 +1,17 @@
-import { global } from "./global";
 
-let cb: (() => void) | null;
-let trigger: (() => void | number);
-
-const executeCallback = (): void => {
-  try {
-    cb && cb();
-  }
-  finally {
-    cb = null;
-  }
-}
-
-// Use MutationObserver to run MicroTask
-// as it's available in all supported browsers
-if ('MutationObserver' in global) {
-  const el = new Text();
-  new MutationObserver(executeCallback).observe(el, { characterData: true });
-  trigger = (): void => {
-    el.textContent = '';
-  }
-}
-else {
-  trigger = (): number => setTimeout(executeCallback);
-}
+let trigger: () => void;
+const callbacks: (() => void)[] = [];
+const notify = (): void => callbacks.splice(0).forEach((cb): void => cb());
 
 const queueMicroTask = (callback: () => void): void => {
-  cb = callback;
+  if (!trigger) {
+    const el = new Text();
+    const config = { characterData: true };
+    new MutationObserver((): void => notify()).observe(el, config);
+    trigger = (): void => { el.textContent = ''; };
+  }
+  callbacks.push(callback);
   trigger();
-}
+};
 
 export { queueMicroTask }
