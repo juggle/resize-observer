5a95329d36b211cf2c977ee8e3eac955611ba7b3
Trem <tremaynechrist@gmail.com>
2019-12-08 11:47:54 +0000

    global scope to return window or basic fallback

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,3 +1,11 @@
+import { ResizeObserver } from '../ResizeObserver';
+import { ResizeObserverEntry } from '../ResizeObserverEntry';
+
+type IsomorphicWindow = Window & {
+  ResizeObserver?: typeof ResizeObserver;
+  ResizeObserverEntry?: typeof ResizeObserverEntry;
+}
+
 /* istanbul ignore next */
-const global = typeof window === 'undefined' ? this || {} : window;
-export { global }
+export const global: IsomorphicWindow =
+typeof window !== 'undefined' ? window : {} as unknown as Window;
