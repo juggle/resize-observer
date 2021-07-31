735398bfdffcb193cfbf8f429122ff193a57e761
Trem <tremaynechrist@gmail.com>
2021-07-31 13:13:40 +0100

    add events list module

---
+++ src/utils/events.ts
@@ -0,0 +1,25 @@
+const windowEvents = [
+  // Global Resize
+  'resize',
+  // Global Load
+  'load',
+]
+
+const documentEvents = [
+  // Transitions & Animations
+  'transitionend',
+  'animationend',
+  'animationstart',
+  'animationiteration',
+  // Interactions
+  'keyup',
+  'keydown',
+  'mouseup',
+  'mousedown',
+  'mouseover',
+  'mouseout',
+  'blur',
+  'focus'
+];
+
+export { windowEvents, documentEvents };
