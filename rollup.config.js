7e79c34ff41b50963ebe558d0681b57f13f49c01
Trem <tremaynechrist@gmail.com>
2021-07-31 15:54:34 +0100

    rollup dist packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
-module.exports = {
-  input: 'lib/exports/resize-observer.js',
+import typescript from '@rollup/plugin-typescript';
+
+module.exports = [{
+  input: 'src/exports/resize-observer.ts',
   output: {
-    file: 'lib/exports/resize-observer.umd.js',
+    file: 'lib/resize-observer.mjs',
+    format: 'esm'
+  },
+  plugins: [typescript()]
+}, {
+  input: 'src/exports/resize-observer.ts',
+  output: {
+    file: 'lib/resize-observer.js',
     format: 'umd',
     name: 'window',
     extend: true,
-  }
-};
+  },
+  plugins: [typescript()]
+}];
